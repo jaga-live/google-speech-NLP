9bbe51fd785e38c69a99a242409472397cdfc2df
Jaga <jagadheesh6@gmail.com>
2023-05-22 23:12:42 +0530

    feat - use regex to extract and correct possible entities

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import "reflect-metadata";
 import { InversifyExpressServer } from "inversify-express-utils";
 import container from "./core/inversify.di";
 import express from "express";
-import "./modules/tensorflow/tensorflow.service";
+// import "./modules/tensorflow/tensorflow.service";
+
 class App {
   async bootstrap() {
     const server = new InversifyExpressServer(container);
--- src/modules/google-speech/google-speech.service.ts
+++ src/modules/google-speech/google-speech.service.ts
@@ -9,9 +9,10 @@ export class GoogleSpeechService extends GCloud {
     };
     const config = {
       encoding: "MP3",
-      sampleRateHertz: 16000,
+      sampleRateHertz: 48000,
       languageCode: "en-US",
     };
+
     const request = {
       audio: audio,
       config: config,
@@ -21,6 +22,67 @@ export class GoogleSpeechService extends GCloud {
     const transcription = response.results
       .map((result) => result.alternatives[0].transcript)
       .join("\n");
+
     console.log(`Transcription: ${transcription}`);
+
+    const extractedEntity = await this.extractEntity(transcription);
+    console.log(extractedEntity);
+
+    return this.extractEntity(transcription);
+  }
+
+  /**Using Regex to extract entities and correct common type errors from Google Speech API
+   *
+   * For Address we may need to use Google NLP API
+   */
+  private async extractEntity(text: string) {
+    const query: any = {};
+
+    /**Email Extraction */
+    const emailRegex =
+      /(?<=email\s+)([A-Za-z0-9\s]+(\.\s+)?)+at\s+([A-Za-z0-9.-]+\.[A-Za-z]{2,})/gi;
+
+    const emailMatch = text.match(emailRegex);
+
+    if (emailMatch && emailMatch.length > 0) {
+      const parts = emailMatch[0].split(" at ");
+      const value = parts[0]
+        .trim()
+        .replace(/\s+hyphen\s+/gi, "-")
+        .replace(/\s+underscore\s+/gi, "_")
+        .replace(/\s/g, "");
+      const domain = parts[1].trim();
+      query.email = `${value.toLowerCase()}@${domain.toLowerCase()}`;
+    }
+
+    /**Phone extraction */
+    const phoneRegex = /(?<=phone\s+)([\d\s]+[\d])/gi;
+    const phoneMatch = text.match(phoneRegex);
+
+    if (phoneMatch && phoneMatch.length > 0) {
+      const phoneNumber = phoneMatch[0].replace(/\s/g, "");
+      query.phone = phoneNumber;
+    }
+
+    /**SSN extraction */
+    const ssnRegex = /(?<=ssn|ssin\s+)([\d\s]+[\d])/gi;
+    const ssnMatch = text.match(ssnRegex);
+
+    if (ssnMatch && ssnMatch.length > 0) {
+      const ssnNumber = ssnMatch[0].replace(/\s/g, "");
+      query.SSN = ssnNumber;
+    }
+
+    /**MRN extraction */
+    const mrnRegex = /(?<=mrn|m r n\s+)([\d\s]+[\d])/gi;
+    const mrnMatch = text.match(mrnRegex);
+
+    if (mrnMatch && mrnMatch.length > 0) {
+      const mrnNumber = mrnMatch[0].replace(/\s/g, "");
+      query.MRN = mrnNumber;
+    }
+    return query;
+
+    /**Address - Todo (With Google NLP) */
   }
 }
